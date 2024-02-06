85284e692e427680ad9fd77dcfc2923b6271575f
Niklas Krieger <53957498+nk-coding@users.noreply.github.com>
2024-02-06 19:47:44 +0100

    Update envelopPlugins.ts
    Co-authored-by: delvh <dev.lh@web.de>

--- envelopPlugins.ts
+++ envelopPlugins.ts
@@ -36,7 +36,7 @@ async function resolveUserAuthenticated(
     }
     let parsedHeader: string;
     if (authHeader.startsWith("Bearer ")) {
-      parsedHeader = authHeader.substring(7);
+      parsedHeader = authHeader.substring("Bearer ".length());
     } else {
       parsedHeader = authHeader;
     }
